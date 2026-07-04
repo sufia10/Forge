f7d936a36ce115d58e224cb51d07fc5a5dd86e5c
Sufia Haseen <sufiahaseen3@gmail.com>
2026-07-04 23:34:13 +0530

    Implement hero section layout

--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,31 +1,38 @@
+import './hero.css'
+
 function Hero(){
     return(
-        <section>
-            <div>
+        <section className = "hero">
+        <div className ='hero-content'>
+            <div className="hero-para">
                 <p> 12,847 projects and counting</p>
             </div>
-
+            
+            <div className = "hero-main"> 
                 <h1> Discover Projects Built by Developers
 
                 </h1>
 
-               <p> The developer community for discovering, 
+               <p className="h-paragraph"> 
+                The developer community for discovering, 
                 sharing, and showcasing software projects.
                  Find inspiration, get feedback, 
                  and connect with builders who ship.
                </p>
-            <div>
+            <div className = "hero-btn">
                 <button> Submit Your Project </button>
                 <button> Explore Projects </button>
              </div>
 
-            <div> 
+            <div className = "hero-stats"> 
                 <p> 8,392 developers</p>
                 <p> 340+ tech stacks</p>
                 <p> 1.2M total upvotes </p>
             
             </div>
-           
+            </div>
+                
+           </div>
         </section>
      
     );
