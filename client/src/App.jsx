9e7d8c23e370f714d4a7fc15520802e3991e2021
Sufia Haseen <sufiahaseen3@gmail.com>
2026-07-05 23:36:06 +0530

    Design Hero section layout and background

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,6 @@
 import AppRoutes from './routes/AppRoutes';
 import Navbar from './components/layout/Navbar';
 
-
 function App(){
   return(
     <div>
--- client/src/components/Home/Hero.jsx
+++ client/src/components/Home/Hero.jsx
@@ -1,15 +1,20 @@
 import './hero.css'
+import {Plus, Flame, ArrowRight, CircleCheck} from "lucide-react";
 
 function Hero(){
     return(
         <section className = "hero">
         <div className ='hero-content'>
+            
             <div className="hero-para">
-                <p> 12,847 projects and counting</p>
+                <p>
+                    <Flame size={18} />
+                    12,847 projects and counting
+                </p>
             </div>
             
             <div className = "hero-main"> 
-                <h1> Discover Projects Built by Developers
+                <h1 className="heading-highlight"> Discover Projects <span>Built by Developers</span>
 
                 </h1>
 
@@ -19,20 +24,41 @@ function Hero(){
                  Find inspiration, get feedback, 
                  and connect with builders who ship.
                </p>
+
             <div className = "hero-btn">
-                <button> Submit Your Project </button>
-                <button> Explore Projects </button>
+                <button className="cta-btn"> 
+                    <Plus size={18} />
+                    Submit Your Project 
+                </button>
+
+                <button className= "sec-btn"> 
+                    Explore Projects
+                    <ArrowRight size={18} />
+                </button>
              </div>
 
             <div className = "hero-stats"> 
+
+                <div className="stats-one">
+                <CircleCheck size={17} />
                 <p> 8,392 developers</p>
+                </div>
+
+                <div className="stats-two">
+                <CircleCheck size={17} />
                 <p> 340+ tech stacks</p>
+                </div>
+
+                <div className="stats-three">
+                <CircleCheck size={17} />
                 <p> 1.2M total upvotes </p>
+                </div>
             
             </div>
             </div>
-                
-           </div>
+                </div>
+            
+           
         </section>
      
     );
