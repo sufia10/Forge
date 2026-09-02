c5b3a8b4da53f9ad9fa5a320719a99bb048d9621
Sufia Haseen <sufiahaseen3@gmail.com>
2026-09-02 20:12:03 +0530

    Add project details banner fade

--- client/src/components/Global/Navbar/Navbar.jsx
+++ client/src/components/Global/Navbar/Navbar.jsx
@@ -1,35 +1,41 @@
-import {Link} from 'react-router-dom'
-import { CodeXml } from 'lucide-react';
-import "./navbar.css"
+import { Link, useLocation } from "react-router-dom";
+import { CodeXml } from "lucide-react";
+import "./navbar.css";
 
-function Navbar(){
-    return(
-        <nav className='navbar'>
-            <div className='navbar-main'>
-                <div className='icon'>
+function Navbar() {
+
+    const location = useLocation();
+
+    const isHome = location.pathname === "/";
+
+    return (
+        <nav className={`navbar ${isHome ? "home-navbar" : "main-navbar"}`}>
+
+            <div className="navbar-main">
+                <div className="icon">
                     <CodeXml />
                 </div>
+
                 <h2>Forge</h2>
             </div>
-    
-            <div className='navbar-links'> 
-                <Link to= '/'> Explore</Link>
-                <Link to ='/projectDetails'>Projects</Link>
-                <Link to = '/profile'>Developers</Link>
-                <Link to ='/dashboard'>Dashboard</Link>
+
+            <div className="navbar-links">
+                <Link to="/">Explore</Link>
+                <Link to="/projectDetails">Projects</Link>
+                <Link to="/profile">Developers</Link>
+                <Link to="/dashboard">Dashboard</Link>
             </div>
-           
-            <div className='navbar-auth'>
-                <Link to = '/Login'> 
-                   Sign In
+
+            <div className="navbar-auth">
+                <Link to="/login">
+                    Sign In
                 </Link>
 
-                 <button> Submit project</button>
+                <button>Submit project</button>
             </div>
-           
+
         </nav>
     );
-    
 }
 
 export default Navbar;
--- client/src/pages/ProjectDetail/ProjectDetails.jsx
+++ client/src/pages/ProjectDetail/ProjectDetails.jsx
@@ -1,6 +1,13 @@
+import projectdetailsbanner from "../../../src/assets/projectdetailsbanner.png";
+import "./projectdetails.css";
+
 function ProjectDetails() {
     return (
-        <div>
+        <div className="project-main">
+            <div className="project-banner">
+               < img src={projectdetailsbanner} alt="Project Details Banner" />
+            </div>
+            
             <h1>Project Details</h1>
         </div>
     );
